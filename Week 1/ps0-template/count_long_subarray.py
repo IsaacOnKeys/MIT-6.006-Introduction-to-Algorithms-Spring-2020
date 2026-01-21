845ba737c77a110a913055eca0bb3dc727a483fb
Isaac <isaaconkeys@protonmail.com>
2026-01-21 11:44:01 +0100

    first commit

---
+++ Week 1/ps0-template/count_long_subarray.py
@@ -0,0 +1,11 @@
+def count_long_subarray(A):
+    """
+    Input:  A     | Python Tuple of positive integers
+    Output: count | number of longest increasing subarrays of A
+    """
+
+
+result = count_long_subarray(
+    (4, 18, 10, 8, 13, 16, 18, 1, 9, 6, 11, 13, 12, 5, 7, 17, 13, 3)
+)
+print(result)
