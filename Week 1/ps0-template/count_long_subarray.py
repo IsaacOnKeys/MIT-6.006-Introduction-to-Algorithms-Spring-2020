8d3cbf70c506fe9112612b0a47e6e2b0e2349640
Isaac <isaaconkeys@protonmail.com>
2026-01-21 17:46:55 +0100

    complete PS0 Problem 6

--- Week 1/ps0-template/count_long_subarray.py
+++ Week 1/ps0-template/count_long_subarray.py
@@ -3,6 +3,22 @@ def count_long_subarray(A):
     Input:  A     | Python Tuple of positive integers
     Output: count | number of longest increasing subarrays of A
     """
+    count = 1
+    longest = 1
+    current_count = 1
+
+    for i in range(1, len(A)):
+        if A[i] > A[i - 1]:
+            current_count += 1
+        else:
+            current_count = 1
+        if current_count == longest:
+            count += 1
+        elif current_count > longest:
+            longest = current_count
+            count = 1
+
+    return count
 
 
 result = count_long_subarray(
